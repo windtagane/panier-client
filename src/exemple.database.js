11de08a24566f42db461b86e048820139756cf79
windtagane <windtagane@gmail.com>
2020-03-05 09:09:31 +0400

    env.database

---
+++ src/exemple.database.js
@@ -0,0 +1,19 @@
+/**
+ * Vous devez créer le fichier env.database.js en suivant ce modèle et en complétant les informations de votre base de donnée locale.
+ */
+
+const DATABASE = {};
+
+DATABASE.LOCAL = {
+    DBNAME: "",
+    USERNAME: "", 
+    PASSWORD: ""
+}
+
+DATABASE.PROD = {
+    DBNAME: "",
+    USERNAME: "",
+    PASSWORD: ""
+}
+
+module.exports = DATABASE;
